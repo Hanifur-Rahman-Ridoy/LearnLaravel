3325bf45e267cf3edfca0c6b4b65f1c64d246830
HRCodex <hrcodexbd@gmail.com>
2023-12-08 11:39:27 +0600

    Laravel Installation

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    {{-- <a href="{{ url('/rdt/') }}" class="btn btn-xs btn-info pull-right">Edit</a> --}}
+    <a href="{{url('/contact',)}}" class="btn btn-xs btn-info pull-right">Contact Page</a>
+    <br>
+    <a href="{{url('/homepage')}}" class="btn btn-xs btn-info pull-right">Home Page Welcome</a>
+</body>
+
+</html>
